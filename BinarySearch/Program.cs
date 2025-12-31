24d650626b0b9ecfc7c4ab490d9ee4ddd8f90465
Hari Shankar Singh <hari@lmg8.tech>
2025-12-31 08:38:49 +0530

    i added one more condition

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -2,33 +2,35 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(binarySearch([1,2,5,6,7,9],2));  //output
-        Console.WriteLine(binarySearch([1, 2, 5, 6, 7, 9], 7));
+        Console.WriteLine(BinarySearch(new int[] { 1, 2, 5, 6, 7, 9 }, 2)); // 1
+        Console.WriteLine(BinarySearch(new int[] { 1, 2, 5, 6, 7, 9 }, 7)); // 4
+        Console.WriteLine(BinarySearch(new int[] { 1, 2, 5, 6, 7, 9 }, 4)); // -1
     }
-    public static int binarySearch(int[] nums,int target)
+    public static int BinarySearch(int[] nums,int target)
     {
         int left = 0;
         int right = nums.Length-1;
         while (left <= right)
         {
-            int mid=(left+right)/2;
+            int mid=left+(right-left)/2; // it will not failed with large value
+           // int mid=(right+left)/2;  this approach is failed with large value
             if(nums[mid] == target)
             {
                 return mid;
             }
             else if(nums[mid] > target)
             {
-                right = mid;
+                right = mid-1;
             }
             else
             {
-                left = mid;
+                left = mid+1;
                 //left++;
                 //
                 //right--;
             }
         }
-        return left;
+        return -1;
 
     }
 }
